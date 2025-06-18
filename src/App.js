a0fddeb4201b97ea427f4c5b1a7bdd5eb77b7420
oktu1234 <fadhlanjais@gmail.com>
2025-06-19 00:26:13 +0800

    Updating app and update frontend app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
 import React from 'react';
 import PolicyForm from './components/PolicyForm';
-import ClaimForm from './components/ClaimForm';
-import ViewPolicy from './components/ViewPolicy';
-import ViewClaim from './components/ViewClaim';
-import VerifyQR from './components/VerifyQR';
-import Dashboard from './components/Dashboard';
+
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <h1><span role="img" aria-label="farm">🌾</span> Crop Insurance Portal</h1>
        <PolicyForm />
-      {/*<ClaimForm />
-      <ViewPolicy />
-      <ViewClaim />
-      <VerifyQR /> */}
-      {/* <Dashboard /> */}
     </div>
   );
 }
